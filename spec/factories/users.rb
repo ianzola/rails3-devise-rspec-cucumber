ec40b1a0800a7f7ba116f44e42aac2c96ac3bb5e
Ingrid Anzola <ingrid@theprojectfactory.com>
2012-09-11 11:56:49 +1000

    Refactored user_spec aiming to take advantages of Rspec capabilities

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,11 +2,12 @@
 
 FactoryGirl.define do
   factory :user do
-    name 'Test User'
-    email 'example@example.com'
-    password 'please'
-    password_confirmation 'please'
+    name 'Example User'
+    email 'user@example.com'
+    password 'foobar'
+    password_confirmation 'foobar'
     # required if the Devise Confirmable module is used
     # confirmed_at Time.now
   end
+  
 end
